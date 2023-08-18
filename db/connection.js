43d9462507cb561bbf5a10aed07f0d6ca07bdd3f
Andra Moraru <andraimoraru>
2023-08-18 17:03:48 +0100

    this commit will update the connection file

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 
-const {PGDATABASE} = process.env;
 const ENV = process.env.NODE_ENV || 'development';
 
 
@@ -8,9 +7,6 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
